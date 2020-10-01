b06e7fe5424602f71c35f53b6f60598311a73a34
Samar khaled <samar.khaled@gmail.com>
2020-10-01 20:56:58 +0200

    Initial Commit

---
+++ DynamicAccessibilityIdentifier/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DynamicAccessibilityIdentifier
+//
+//  Created by Samar Khaled on 1/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

0602e92eb490e9c61d59ff9157e1579d1db6e5df
Samar khaled <samar.khaled@gmail.com>
2020-10-01 21:55:43 +0200

    POC dynamicSetAccessibilityIdentifier

--- DynamicAccessibilityIdentifier/ViewController.swift
+++ DynamicAccessibilityIdentifier/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var helloLabel: UILabel!
+    @IBOutlet var switchSteps: UISegmentedControl!
+    var testString: String = "test"
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        dynamicSetAccessibilityIdentifier()
+        print("view.accessibilityIdentifier = \(self.view.accessibilityIdentifier as Any)")
+        print("helloLabel.accessibilityIdentifier = \(helloLabel.accessibilityIdentifier as Any)")
+        print("switchSteps.accessibilityIdentifier = \(switchSteps.accessibilityIdentifier as Any)")
     }
-
 
 }
 
+protocol AccessibilityIdentifierViewController where Self: UIViewController {
+    func dynamicSetAccessibilityIdentifier()
+}
+
+extension UIViewController :AccessibilityIdentifierViewController {
+    func dynamicSetAccessibilityIdentifier() {
+        let m = Mirror(reflecting: self)
+        print(m.description)
+        self.view.accessibilityIdentifier = m.description
+            
+        print(m.children.count)
+        for p in m.children {
+            guard let control = p.value as? UIView else {
+                return
+            }
+            control.accessibilityIdentifier = p.label
+        }
+    }
+}
